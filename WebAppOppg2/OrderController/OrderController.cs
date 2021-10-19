c19696013c36d9f5ef73dafe7bb5c972fd8f7802
helenerogne <s344241@oslomet.no>
2021-10-19 13:41:53 +0200

    controller

---
+++ WebAppOppg2/OrderController/OrderController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebAppOppg2.OrderController
+{
+    [Route("api/[controller]")]
+    public class OrderController
+    {
+        /*
+        private ILogger<OrderController> _log;
+        private readonly ITicketRepository _db;
+
+        public OrderController(ITicketRepository db, ILogger<OrderController> log)
+        {
+            _db = db;
+            _log = log;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> SaveOrder(Ticket inTicket)
+        {
+            bool returOK = await _db.saveOrder(inTicket);
+            if (!returOK)
+            {
+                _log.LogInformation("Bestilling ble ikke lagret");
+                return BadRequest("Bestilling ble ikke lagret");
+            }
+            return Ok(inTicket);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetOne(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                Ticket ticket = await _db.GetOne(id);
+                if (ticket == null)
+                {
+                    _log.LogInformation("Fant ikke kunden");
+                    return NotFound("Fant ikke kunden");
+                }
+                return Ok(ticket);
+            }
+            _log.LogInformation("Feil i inputvalidering");
+            return BadRequest("Feil i inputvalidering på server");
+        }
+
+        */
+
+    }
+}
