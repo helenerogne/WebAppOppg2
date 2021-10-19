c3494f5b9952ef902f57433eddb9b561b87f7b87
helenerogne <s344241@oslomet.no>
2021-10-19 13:38:27 +0200

    lagt til klasser

---
+++ WebAppOppg2/ClientApp/src/app/admin/admin.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Ticket } from "../Ticket";
+
+@Component({
+  templateUrl: "admin.component.html"
+})
+export class admin.component.ts {
+  allTickets: Array<Ticket>;
+  loading: boolean;
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  ngOnInit() {
+    this.loading = true;
+    this.getAllTickets();
+  }
+
+  getAllTickets() {
+    this.http.get<Ticket[]>("api/ticket/")
+      .subscribe(tickets => {
+        this.allTickets = tickets;
+        this.loading = false;
+      },
+        error => console.log(error)
+      );
+  };
+
+  deleteTicket(id: number) {
+    this.http.delete("api/ticket/" + id)
+      .subscribe(retur => {
+        this.getAllTickets();
+        this.router.navigate(['/admin.component.html']);
+      },
+        error => console.log(error)
+      );
+  };
+}
