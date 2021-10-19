f33da4b94f77b2ba22820bab01341b73e3aac643
Carina <carinawoldvik@gmail.com>
2021-10-19 15:33:19 +0200

    lagt inn Repository, DBinit, TicketDB

---
+++ WebAppOppg2/DAL/TicketDB.cs
@@ -0,0 +1,38 @@
+﻿using WebAppOppg2.Models;
+
+namespace WebAppOppg2.DAL
+{
+
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class Tickets
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Route { get; set; }
+        public string Date { get; set; }
+        public int Count { get; set; }
+        public string Type { get; set; }
+        public string Price { get; set; }
+    }
+
+
+    public class TicketDB : DbContext
+    {
+        public TicketDB(DbContextOptions<TicketDB> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+    }
+}
