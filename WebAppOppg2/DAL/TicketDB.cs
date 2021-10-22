4f37df73294d06b4ea497b19da4bda0b74356cd9
Carina <carinawoldvik@gmail.com>
2021-10-22 14:46:34 +0200

    oppdatert DB med nye tabeller og endret entiteter i Ticket

--- WebAppOppg2/DAL/TicketDB.cs
+++ WebAppOppg2/DAL/TicketDB.cs
@@ -7,14 +7,49 @@ namespace WebAppOppg2.DAL
     public class Ticket
     {
         public int ID { get; set; }
+        public Passenger Passenger { get; set; }
+        public Route Route { get; set; }
+        public string Date { get; set; }
+
+    }
+
+    public class Port
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class TravelType
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Route
+    {
+        public int ID { get; set; }
+        public TravelType TravelType { get; set; }
+        public Port PortFrom { get; set; }
+        public Port PortTo { get; set; }
+        public int Price { get; set; }
+        public string DepartureOption1 { get; set; }
+        public string DepartureOption2 { get; set; }
+    }
+
+    public class Passenger
+    {
+        public int ID { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string Email { get; set; }
-        public string Route { get; set; }
-        public string Date { get; set; }
-        public int Quantity { get; set; }
-        public string Type { get; set; }
-        public string Price { get; set; }
+        public PassengerType PassengerType { get; set; }
+    }
+
+    public class PassengerType
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Discount { get; set; }
     }
 
 
@@ -27,5 +62,10 @@ namespace WebAppOppg2.DAL
         }
 
         public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Port> Ports { get; set; }
+        public DbSet<TravelType> TravelTypes { get; set; }
+        public DbSet<Route> Routes { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<PassengerType> PassengerTypes { get; set; }
     }
 }
--- WebAppOppg2/Models/Ticket.cs
+++ WebAppOppg2/Models/Ticket.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,17 +11,66 @@ namespace WebAppOppg2.Models
     {
         [Key]
         public int ID { get; set; }
+        [ForeignKey("Passenger")]
+        public Passenger Passenger { get; set; }
+        [ForeignKey("Route")]
+        public Route Route { get; set; }
+        public string Date { get; set; }
+    }
+
+    public class Port
+    {
+        [Key]
+        public int ID { get; set; }
+        [RegularExpression(@"[a-zA-ZæøåÆØÅ. \-]{2,20}")]
+        public string Name { get; set; }
+    }
+
+    public class TravelType
+    {
+        [Key]
+        public int ID { get; set; }
+        [RegularExpression(@"[a-zA-ZæøåÆØÅ. \-]{2,20}")]
+        public string Name { get; set; }
+    }
+
+    public class Route
+    {
+        [Key]
+        public int ID { get; set; }
+        [ForeignKey("TravelType")]
+        public TravelType TravelType { get; set; }
+        [ForeignKey("Port")]
+        public Port PortFrom { get; set; }
+        [ForeignKey("Port")]
+        public Port PortTo { get; set; }
+        [RegularExpression(@"[0-9]{1,100}")]
+        public int Price { get; set; }
+        public string DepartureOption1 { get; set; }
+        public string DepartureOption2 { get; set; }
+    }
+
+    public class Passenger
+    {
+        [Key]
+        public int ID { get; set; }
         [RegularExpression(@"[a-zA-ZæøåÆØÅ. \-]{2,20}")]
         public string Firstname { get; set; }
         [RegularExpression(@"[a-zA-ZæøåÆØÅ. \-]{2,20}")]
         public string Lastname { get; set; }
         [RegularExpression(@"[a-zA-ZæøåÆØÅ. \-]{2,50}")]
         public string Email { get; set; }
-        public string Route { get; set; }
-        public string Date { get; set; }
-        public int Quantity { get; set; }
+        [ForeignKey("PassengerType")]
+        public PassengerType PassengerType { get; set; }
+    }
+
+    public class PassengerType
+    {
+        [Key]
+        public int ID { get; set; }
+        [RegularExpression(@"[a-zA-ZæøåÆØÅ. \-]{2,20}")]
+        public string Name { get; set; }
         [RegularExpression(@"[0-9]{1,100}")]
-        public string Type { get; set; }
-        public string Price { get; set; }
+        public int Discount { get; set; }
     }
 }
