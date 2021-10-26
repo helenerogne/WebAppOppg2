ff1c1c5849d89d433d8e14f8f1ec4702b6922392
Marianne Storhoug <s336188@gmail.com>
2021-10-26 10:28:13 +0200

    Kommentert ut MockHttpSession.

--- UnitTestProject/MockHttpSession.cs
+++ UnitTestProject/MockHttpSession.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿/*
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+
+*/
 
 /*
  Kode hentet fra Canvas-modul "Enhetstest av KundeApp-logginn løsning" (i faget ITPE3200-1 21H).
