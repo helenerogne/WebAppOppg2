dbe6452ad80cc6de28573aa93e70c19d19810dff
Carina <carinawoldvik@gmail.com>
2021-10-29 18:30:59 +0200

    prøvde å legge inn logger, men fikk feil

--- WebAppOppg2/Startup.cs
+++ WebAppOppg2/Startup.cs
@@ -44,11 +44,12 @@ namespace WebAppOppg2
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                //loggerFactory.AddFile("Logs/Ticketlog.txt");
                 DBInit.Initialize(app);
             }
             else
