f1c82297e1e1b7b270ce10ef9d4a56031a4d11ad
Marianne Storhoug <s336188@gmail.com>
2021-11-01 22:04:45 +0100

    Wonky adminLogin + testingfiler

--- WebAppOppg2/ClientApp/src/app/adminLogin/adminLogin.component.ts
+++ WebAppOppg2/ClientApp/src/app/adminLogin/adminLogin.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
-  templateUrl: './adminLogin.component.html'
+  templateUrl: 'adminLogin.component.html'
 })
 
 export class AdminLoginComponent {
+  LoginForm: FormGroup;
 
+  // fb kan endres til et annet logisk navn
+  constructor(private fb: FormBuilder) {
+    this.LoginForm = fb.group({
+      username: ["", Validators.required],
+      password: ["", Validators.pattern("[0-9]{6,15}")]
+    });
+  }
+
+  onSubmit() {
+    console.log("LoginForm innsendt:");
+    console.log(this.LoginForm);
+    console.log(this.LoginForm.value.brukernavn);
+    console.log(this.LoginForm.touched);
+  }
 }
